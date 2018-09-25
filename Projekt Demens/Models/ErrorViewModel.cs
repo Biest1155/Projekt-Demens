cec44c8b1064cf8732817dcfb62a1470eb14a162
Biest1155 <mikk934a@edu.eal.dk>
2018-09-25 08:47:13 +0200

    Add project files.

---
+++ Projekt Demens/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projekt_Demens.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
