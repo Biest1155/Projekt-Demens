f5f6d18d4945bd0ee61d1fcf411c45e97be2c0bb
Natasha Drost <nata4471@edu.eal.dk>
2018-09-26 17:19:53 +0200

    ChatMessageKlasse

---
+++ Projekt Demens/Models/ChatMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_Demens.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public bool TerapeutIsAuthor { get; set; }
+        public int TerapeutId { get; set; }
+        public int PatientId { get; set; }
+        public string Content { get; set; }
+        public DateTime Posted { get; set; }
+       
+
+        //Når jeg går ind på chatsiden skal jeg bruge navnene på patienterne, ikke bare Id'et.
+        //Denne information skal hentes fra en anden klasse.
+    }
+}
