eb14870a8a6b8d566e70fc3d258e0bf5c1088229
Natasha Drost <nata4471@edu.eal.dk>
2018-09-27 11:02:40 +0200

    Flytning af chatten til TherapistController

--- Projekt Demens/Controllers/TherapistController.cs
+++ Projekt Demens/Controllers/TherapistController.cs
@@ -3,14 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Projekt_Demens.Models;
 
 namespace Projekt_Demens.Controllers
 {
     public class TherapistController : Controller
     {
-        public IActionResult Index()
+        public List<ChatMessage> AllMessages = new List<ChatMessage>()
         {
-            return View();
+            new ChatMessage()
+            {
+                Id = 1, PatientId = 1, Content = "Jeg har fået problemer med at huske alfabetet", Posted = new DateTime(2017,05,20),
+                TerapeutId = 1, TerapeutIsAuthor = false
+            },
+            new ChatMessage()
+            {
+                Id = 2, PatientId = 1, TerapeutId = 1, Content = "Har du lavet øvelserne", TerapeutIsAuthor = true, Posted = new DateTime(2017,07,04)
+            },
+
+            new ChatMessage()
+            {
+                Id = 3, PatientId = 1, TerapeutId = 1, Content = "Nej", TerapeutIsAuthor = false, Posted = new DateTime(2017,08,14)
+            },
+            new ChatMessage()
+            {
+                Id=4, PatientId = 2, TerapeutId = 1, Content = "Jeg glemte koden til Dankortet idag", TerapeutIsAuthor = false, Posted = new DateTime(2018,01,04)
+            },
+            new ChatMessage()
+            {
+                Id = 5, PatientId = 3, Content = "Jeg vil anbefale at du diskuterer øvelsen imorgen med din mand", TerapeutIsAuthor = true, TerapeutId = 1, Posted = new DateTime(2018,02,14)
+            }
+
+        };
+        public IActionResult Index(int id=1)
+        {
+            List<ChatMessage> MessageByPatient = new List<ChatMessage>();
+            foreach (var i in AllMessages)
+            {
+                if (i.PatientId == id)
+                {
+                    MessageByPatient.Add(i);
+                }
+            }
+            return View(MessageByPatient);
         }
 
         public IActionResult News()
