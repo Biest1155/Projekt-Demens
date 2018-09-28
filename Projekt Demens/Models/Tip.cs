fd8296f4ca85bed99c19192c5b5fa6bd6d1ca754
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-09-28 14:37:08 +0200

    Added Tip model

---
+++ Projekt Demens/Models/Tip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_Demens.Models
+{
+    public class Tip
+    {
+        public enum TipType { patient, relative}
+        public string Title { get; set; }
+        public string Body { get; set; } 
+        public TipType Type { get; set; }
+    }
+}
