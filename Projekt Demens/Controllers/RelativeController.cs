448c32a092eda20abbb1bbc098671a22e93fa78b
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-10-10 09:15:13 +0200

    Added data context

--- Projekt Demens/Controllers/RelativeController.cs
+++ Projekt Demens/Controllers/RelativeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Projekt_Demens.Models;
 
 namespace Projekt_Demens.Controllers
 {
     public class RelativeController : Controller
     {
+        private readonly DataContext _db;
+
+        public RelativeController(DataContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
